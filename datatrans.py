e178cd2aa30014e389e2c41de71e66692ebba056
aljinny <7inny27th@naver.com>
2022-05-20 05:08:36 +0900

    plop05

--- datatrans.py
+++ datatrans.py
@@ -6,7 +6,7 @@ curs = conn.cursor()
 #curs.execute("CREATE TABLE bugs_pl (id INTEGER ,pl_title TEXT, li_id INTEGER)")
 # TABLE : measures , 컬럼이름 : (timestamp , measure)
 
-reader = csv.reader(open('testlist.csv', 'r'))  # CSV파일 읽기모드로 열기
+reader = csv.reader(open('pl_final.csv', 'r'))  # CSV파일 읽기모드로 열기
 for row in reader:  # for 반복문을 통하여 DB에 작성
     to_db = [(row[0]), (row[1]), (row[2]), (row[3]), (row[4])]
     curs.execute("INSERT INTO bugs_pl (id, pl_id,s_title,s_artist,plop_id) VALUES (?, ?,?,?,?);", to_db)
